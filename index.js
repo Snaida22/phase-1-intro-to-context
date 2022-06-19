395ff18e282c260bf4c69281e0d0bca5139ed279
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-19 16:50:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function createEmployeeRecord([str1, str2,str3, str4],){
+    return {
+        firstName: str1,
+        familyName: str2,
+        title: str3,
+        payPerHour: str4,
+        timeInEvents: [],
+        timeOutEvents: []
+        
+     }
+
+}
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(createEmployeeRecord){
+        return createEmployeeRecords(createEmployeeRecord)
+    })
+}
